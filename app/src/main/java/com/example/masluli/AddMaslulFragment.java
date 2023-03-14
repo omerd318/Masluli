25929b55ea4a1c3d72d41f3e549c3b503db11cea
omerd318 <omer.damari@gmail.com>
2023-03-15 00:21:58 +0200

    Add(AddMaslul): add maslul screen UI

--- app/src/main/java/com/example/masluli/AddMaslulFragment.java
+++ app/src/main/java/com/example/masluli/AddMaslulFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link AddRouteFragment#newInstance} factory method to
+ * Use the {@link AddMaslulFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class AddRouteFragment extends Fragment {
+public class AddMaslulFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class AddRouteFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public AddRouteFragment() {
+    public AddMaslulFragment() {
         // Required empty public constructor
     }
 
@@ -37,8 +37,8 @@ public class AddRouteFragment extends Fragment {
      * @return A new instance of fragment AddRouteFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static AddRouteFragment newInstance(String param1, String param2) {
-        AddRouteFragment fragment = new AddRouteFragment();
+    public static AddMaslulFragment newInstance(String param1, String param2) {
+        AddMaslulFragment fragment = new AddMaslulFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class AddRouteFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_route, container, false);
+        return inflater.inflate(R.layout.fragment_add_maslul, container, false);
     }
 }
