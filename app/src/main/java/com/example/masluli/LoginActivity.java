f4339865d90b559d1d79511962716e369dc5c40a
omerd318 <omer.damari@gmail.com>
2023-03-18 16:36:23 +0200

    Fix(action_bar): fix back navigation from sign-up screen

--- app/src/main/java/com/example/masluli/LoginActivity.java
+++ app/src/main/java/com/example/masluli/LoginActivity.java
@@ -1,11 +1,15 @@
 package com.example.masluli;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDirections;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class LoginActivity extends AppCompatActivity {
     NavController navController;
@@ -19,5 +23,20 @@ public class LoginActivity extends AppCompatActivity {
         navController = navHostFragment.getNavController();
         NavigationUI.setupActionBarWithNavController(this,navController);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_action_bar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == android.R.id.home) {
+            navController.popBackStack();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
